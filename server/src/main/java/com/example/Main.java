88d8d8ecea0eaae8fc50d1e65f8c67de7b0b37b6
lai2-24 <lai2-24@itismeucci.com>
2025-11-11 11:25:35 +0100

    first commit

---
+++ server/src/main/java/com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final int PORTA = 3000;
+        ServerSocket ss = new ServerSocket(PORTA);
+        Socket s = ss.accept();
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+    }
+}
